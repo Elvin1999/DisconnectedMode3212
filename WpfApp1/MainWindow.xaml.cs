e5e41181a2cf15d6b659b835fb442ad49797c34a
Elvin Jamalzade <camalzadeelvin@itstep.edu.az>
2022-11-26 17:36:54 +0400

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SqlConnection conn;
+        string cs = "";
+        DataTable table;
+        SqlDataReader reader;
+        public MainWindow()
+        {
+            InitializeComponent();
+            conn = new SqlConnection();
+            cs = ConfigurationManager.ConnectionStrings["MyConnString"].ConnectionString;
+
+
+            using (conn = new SqlConnection())
+            {
+                da = new SqlDataAdapter();
+                conn.ConnectionString = cs;
+                conn.Open();
+                set = new DataSet();
+
+                SqlCommand command = new SqlCommand("SELECT * FROM Authors", conn);
+
+                //command.Parameters.Add(new SqlParameter
+                //{
+                //    DbType = DbType.Int32,
+                //    ParameterName = "@id",
+                //    Value = 1
+                //});
+
+                da.SelectCommand = command;
+
+                da.Fill(set, "firstAuthor");
+                myDataGrid1.ItemsSource = set.Tables[0].DefaultView;
+            }
+            }
+        DataSet set;
+        SqlDataAdapter da;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            #region DataTable
+
+            //using (var conn=new SqlConnection())
+            //{
+            //    conn.ConnectionString = cs;
+            //    conn.Open();
+
+            //    SqlCommand command = new SqlCommand();
+            //    command.CommandText = "SELECT * FROM Authors";
+            //    command.Connection = conn;
+
+            //    table = new DataTable();
+
+            //    bool hasColumnAdded = false;
+            //    using (reader=command.ExecuteReader())
+            //    {
+            //        while (reader.Read())
+            //        {
+            //            if (!hasColumnAdded)
+            //            {
+            //                for (int i = 0; i < reader.FieldCount; i++)
+            //                {
+            //                    table.Columns.Add(reader.GetName(i));
+            //                }
+            //                hasColumnAdded = true;
+            //            }
+            //            DataRow row=table.NewRow();
+            //            for (int i = 0; i < reader.FieldCount; i++)
+            //            {
+            //                row[i] = reader[i];
+            //            }
+            //            table.Rows.Add(row);
+            //        }
+
+            //        myDataGrid1.ItemsSource=table.DefaultView;
+            //    }
+
+            //}
+
+            #endregion
+
+
+
+            #region DataSet And SqlDataAdapter
+
+            //using (conn=new SqlConnection())
+            //{
+            //    conn.ConnectionString = cs;
+            //    conn.Open();
+            //    set = new DataSet();
+
+            //    da=new SqlDataAdapter("SELECT * FROM Authors;SELECT * FROM Books",conn);
+
+            //    da.Fill(set, "mybook");
+
+            //    myDataGrid1.ItemsSource = set.Tables[0].DefaultView;
+            //    myDataGrid2.ItemsSource = set.Tables[1].DefaultView;
+
+
+            //}
+
+
+            using (conn = new SqlConnection())
+            {
+                da = new SqlDataAdapter();
+                conn.ConnectionString = cs;
+                conn.Open();
+                set = new DataSet();
+
+                SqlCommand command = new SqlCommand("SELECT * FROM Authors", conn);
+
+                //command.Parameters.Add(new SqlParameter
+                //{
+                //    DbType = DbType.Int32,
+                //    ParameterName = "@id",
+                //    Value = 1
+                //});
+
+                da.SelectCommand = command;
+
+                da.Fill(set, "firstAuthor");
+
+            //    myDataGrid1.ItemsSource = set.Tables[0].DefaultView;
+
+
+
+
+                //da = new SqlDataAdapter();
+                //conn.ConnectionString = cs;
+                //conn.Open();
+                //set = new DataSet();
+
+                command = new SqlCommand("UPDATE Authors SET Firstname=@firstName WHERE Id=@id", conn);
+
+                command.Parameters.Add(new SqlParameter
+                {
+                    DbType = DbType.Int32,
+                    ParameterName = "@id",
+                    Value = 1
+                });
+
+                command.Parameters.Add(new SqlParameter
+                {
+                    SqlDbType = SqlDbType.NVarChar,
+                    ParameterName = "@firstName",
+                    Value = "DDDDD"
+                });
+
+                da.UpdateCommand = command;
+                da.UpdateCommand.ExecuteNonQuery();
+
+                da.Update(set, "firstAuthor");
+                set.Clear();
+                da.Fill(set, "firstAuthor");
+
+               // myDataGrid1.ItemsSource = null;
+                myDataGrid1.ItemsSource = set.Tables[0].DefaultView;
+
+
+            }
+
+
+            #endregion
+        }
+    }
+}
